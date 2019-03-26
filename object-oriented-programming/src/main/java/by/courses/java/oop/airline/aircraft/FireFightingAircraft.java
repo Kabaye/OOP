4888c656d0ad853f6a0e19e050e7cb83f8e15868
Kabaye <svkulich@gmail.com>
2019-03-26 23:18:23 +0300

    feat: change logic of class

--- object-oriented-programming/src/main/java/by/courses/java/oop/airline/aircraft/FireFightingAircraft.java
+++ object-oriented-programming/src/main/java/by/courses/java/oop/airline/aircraft/FireFightingAircraft.java
@@ -1,21 +1,20 @@
 package by.courses.java.oop.airline.aircraft;
 
 //Самолет экстренных ситуаций (тушить пожары)
-public class FireFightingAircraft extends CargoAircraft {
+public class FireFightingAircraft extends AbstractAircraft {
     private int integratedBoilersVolume; //объем встроенных баков с водой
     private boolean availabilityToHaveExternalBoiler;
 
-    public FireFightingAircraft(Integer range, Integer maximumSpeed, String model, String producer, Integer cargoCompartmentVolume, Integer capacity, int integratedBoilersVolume, boolean availabilityToHaveExternalBoiler) {
-        super(range, maximumSpeed, model, producer, cargoCompartmentVolume, capacity);
+    public FireFightingAircraft(Integer range, Integer maximumSpeed, String model, String producer, int integratedBoilersVolume, boolean availabilityToHaveExternalBoiler) {
+        super(range, maximumSpeed, model, producer);
         this.integratedBoilersVolume = integratedBoilersVolume;
         this.availabilityToHaveExternalBoiler = availabilityToHaveExternalBoiler;
     }
 
     @Override
     public String toString() {
-        String str = super.toString();
-        return str.substring(0, str.length() - 2) +
-                ", volume of integrated boilers = " + integratedBoilersVolume +
+        return super.toString() +
+                "volume of integrated boilers = " + integratedBoilersVolume +
                 "m^3, available to have external boiler = " + availabilityToHaveExternalBoiler + "}";
     }
 }
