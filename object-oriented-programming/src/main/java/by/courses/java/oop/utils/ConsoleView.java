6760c96badae336d02a6a3ea972e7bda7a4d66a4
Kabaye <svkulich@gmail.com>
2019-03-26 23:01:19 +0300

    feat: delete "logically" repeated methods

--- object-oriented-programming/src/main/java/by/courses/java/oop/utils/ConsoleView.java
+++ object-oriented-programming/src/main/java/by/courses/java/oop/utils/ConsoleView.java
@@ -1,23 +1,21 @@
 package by.courses.java.oop.utils;
 
-import by.courses.java.oop.airline.aircraft.AbstractAircraft;
-import by.courses.java.oop.airline.aircraft.CargoAircraft;
-import by.courses.java.oop.airline.aircraft.PassengerAircraft;
 import by.courses.java.oop.airline.Airline;
+import by.courses.java.oop.airline.aircraft.AbstractAircraft;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class ConsoleView {
-    private final static String BEGINNING = "Hello! Lets start!";
-    private final static String COMMANDS = "Your commands: \n" +
+    public final static String BEGINNING = "Hello! Lets start!";
+    public final static String COMMANDS = "Your commands: \n" +
             "1 --- print info about airline;\n" +
             "2 --- open add menu;\n" +
             "3 --- count total capacity and amount of seats;\n" +
             "4 --- sort airline's aircrafts by range;\n" +
             "5 --- search menu;\n" +
             "6 --- exit;";
-   private final static String SEARCH = "Please, write min and max speed and range, we will find aircraft, which approach selected parameters";
-    private final static String ADD = "Please, choose what type of aircraft you want to add:\n" +
+    public final static String SEARCH = "Please, write min and max speed and range, we will find aircraft, which approach selected parameters";
+    public final static String ADD = "Please, choose what type of aircraft you want to add:\n" +
             "1 --- passenger aircraft;\n" +
             "2 --- cargo aircraft;\n" +
             "3 --- fire fighter aircraft. Your choice: ";
@@ -36,16 +34,16 @@ public class ConsoleView {
 
     public static void start() {
         System.out.println(BEGINNING);
-        printCommands();
+        printCommands(COMMANDS);
     }
 
-    public static void printCommands() {
-        System.out.println(COMMANDS);
+    public static void printCommands(String str) {
+        System.out.println(str);
     }
 
-    public static void error() {
+    public static void error(String str) {
         System.out.println("Please, write just menu commands!");
-        printCommands();
+        printCommands(str);
     }
 
     public static void close(Scanner input) {
@@ -53,23 +51,11 @@ public class ConsoleView {
         input.close();
     }
 
-    public static void searchMenu() {
-        System.out.println(SEARCH);
-    }
-
-    public static void printSearchedAircraft(AbstractAircraft aircraft){
-        if (aircraft == null)
-        {
+    public static void printSearchedAircraft(AbstractAircraft aircraft) {
+        if (aircraft == null) {
             System.out.println("There is no such aircraft, which approach selected parameters!");
-        }
-        else
-        {
+        } else {
             System.out.println("We found this aircraft: " + aircraft);
         }
-    }
-
-    public static void searchError(Scanner input) {
-        System.out.println("Please, write just numbers!");
-        input.nextLine();
     }
 }
