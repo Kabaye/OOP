5e822359441e0d86aad145168f7d60d352489cbc
Kabaye <svkulich@gmail.com>
2019-03-26 22:22:58 +0300

    fix: change access modifiers for constants to private

--- object-oriented-programming/src/main/java/by/courses/java/oop/utils/ConsoleView.java
+++ object-oriented-programming/src/main/java/by/courses/java/oop/utils/ConsoleView.java
@@ -8,16 +8,16 @@ import by.courses.java.oop.airline.Airline;
 import java.util.*;
 
 public class ConsoleView {
-    public final static String BEGINNING = "Hello! Lets start!";
-    public final static String COMMANDS = "Your commands: \n" +
+    private final static String BEGINNING = "Hello! Lets start!";
+    private final static String COMMANDS = "Your commands: \n" +
             "1 --- print info about airline;\n" +
             "2 --- open add menu;\n" +
             "3 --- count total capacity and amount of seats;\n" +
             "4 --- sort airline's aircrafts by range;\n" +
             "5 --- search menu;\n" +
             "6 --- exit;";
-    public final static String SEARCH = "Please, write min and max speed and range, we will find aircraft, which approach selected parameters";
-    public final static String ADD = "Please, choose what type of aircraft you want to add:\n" +
+   private final static String SEARCH = "Please, write min and max speed and range, we will find aircraft, which approach selected parameters";
+    private final static String ADD = "Please, choose what type of aircraft you want to add:\n" +
             "1 --- passenger aircraft;\n" +
             "2 --- cargo aircraft;\n" +
             "3 --- fire fighter aircraft. Your choice: ";
